ab20714b68ba8c514bf8d092657da4b8610b285c
JF Ding <jfding@gmail.com>
2025-08-19 13:51:28 +0800

    upgrade base docker image and add label for each update
    Signed-off-by: JF Ding <jfding@gmail.com>

--- _docker/build.sh
+++ _docker/build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+TODAY=$(date +%Y%m%d)
+
 cd ../gh-webhook/
 uv build
 cp dist/*.whl ../_docker/
 cp hook.py ../_docker/
 
 cd ../_docker/
-docker build -t rushiai/auto-reloader .
+docker build -t rushiai/auto-reloader:$TODAY .
 
 rm -f *.whl hook.py
